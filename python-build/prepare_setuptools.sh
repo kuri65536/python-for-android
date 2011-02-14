9a34dab01f98acb11c11ef266ed8dbf47a308766
Naranjo Manuel Francisco <manuel@aircable.net>
2011-02-14 11:09:58 -0300

    improved script so it exports variables correctly

--- python-build/prepare_setuptools.sh
+++ python-build/prepare_setuptools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH=$PATH:$ANDROID_NDK_TOOLCHAIN_ROOT/bin
-export PY4A_INC=$PY4A/include
-export PY4A_LIB=$PY4A/lib
-export PYTHONPATH=$PYTHONPATH:$PY4A
+
+export PY4A=`dirname $BASH_SOURCE`
+export PATH="${PATH}:${ANDROID_NDK_TOOLCHAIN_ROOT}/bin"
+export PY4A_INC="${PY4A}/include"
+export PY4A_LIB="${PY4A}/lib"
+export PYTHONPATH="${PYTHONPATH}:${PY4A}"
