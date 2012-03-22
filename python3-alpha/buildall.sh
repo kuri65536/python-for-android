5744680a453b8c2b519ff3b1541ac0f6f9410c14
Robbie Matthews <rjmatthews62@gmail.com>
2012-03-22 14:22:04 +1100

    Build all script

---
+++ python3-alpha/buildall.sh
@@ -0,0 +1,18 @@
+echo "Building all Py34a"
+echo "openssl"
+pushd openssl
+./xbuild.sh
+popd
+echo "Sqlite3"
+pushd sqlite3
+./xbuild.sh
+popd
+echo "zlip"
+pushd zlib-1.2.6
+./xbuild.sh
+popd
+echo "Python 3"
+pushd python3-src
+./xbuild.sh
+popd
+echo "And done."
