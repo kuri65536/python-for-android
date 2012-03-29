d94d57445dbef6dba36551fc2297c5571edff5c5
Robbie Matthews <rjmatthews62@gmail.com>
2012-03-29 22:09:21 +1100

    Shared Library build

---
+++ python3-alpha/readline-6.2/xconf.sh
@@ -0,0 +1,7 @@
+export 	TARGET=arm-linux-androideabi
+pushd ../thirdparty
+TARGET_DIR=`pwd`
+popd
+
+LDFLAGS=-L${TARGET_DIR}/lib CFLAGS="-I${TARGET_DIR}/include -I${TARGET_DIR}/include/ncurses" ./configure --host=$TARGET --target=$TARGET --prefix=$TARGET_DIR --enable-shared  --with-curses && \
+ patch <android.patch
