f666722b537305778001b71254634d4a708c40f3
shimoda@desktop <shimoda@desktop>
2015-03-22 09:35:27 +0900

    Added support for relative path. this patch will allow execuatable to execute in local directory thereby allowing relative directory referencing.

--- python-build/standalone_python.sh
+++ python-build/standalone_python.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
-
+PW=`pwd`
 export EXTERNAL_STORAGE=/mnt/storage
+export LANG=en
 PYTHONPATH=/mnt/storage/com.googlecode.pythonforandroid/extras/python
 PYTHONPATH=${PYTHONPATH}:/data/data/com.googlecode.pythonforandroid/files/python/lib/python2.7/lib-dynload
 export PYTHONPATH
@@ -8,4 +9,5 @@ export TEMP=/mnt/storage/com.googlecode.pythonforandroid/extras/python/tmp
 export PYTHON_EGG_CACHE=$TEMP
 export PYTHONHOME=/data/data/com.googlecode.pythonforandroid/files/python
 export LD_LIBRARY_PATH=/data/data/com.googlecode.pythonforandroid/files/python/lib
+cd $PW
 /data/data/com.googlecode.pythonforandroid/files/python/bin/python "$@"
