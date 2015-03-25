ce2c4929c8f2db6cb6dd9cc59115f631592ac38f
shimoda@desktop <shimoda@desktop>
2015-03-26 00:31:28 +0900

    Success to compile readline-6.3 (python3 not work now)

--- python3-alpha/readline-6.2/xconf.sh
+++
@@ -1,7 +0,0 @@
-export 	TARGET=arm-linux-androideabi
-pushd ../thirdparty
-TARGET_DIR=`pwd`
-popd
-
-LDFLAGS=-L${TARGET_DIR}/lib CFLAGS="-I${TARGET_DIR}/include -I${TARGET_DIR}/include/ncurses" ./configure --host=$TARGET --target=$TARGET --prefix=$TARGET_DIR --enable-shared  --with-curses && \
- patch <android.patch
