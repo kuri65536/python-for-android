3f5011dd966e748df3c6157f958c55ce63a0b0b1
shimoda@desktop <shimoda@desktop>
2015-03-25 23:12:10 +0900

    Success to compile ncurses-5.9 (python3 not work now)

--- python3-alpha/ncurses-5.9/xbuild.sh
+++
@@ -1,9 +0,0 @@
-export TARGET=arm-linux-androideabi
-pushd ../thirdparty
-TARGET_DIR=`pwd`
-popd
-./configure --host=$TARGET --target=$TARGET --prefix=$TARGET_DIR --with-shared && \
-patch -p1 <android.patch && \
-make && \
-make install
-
