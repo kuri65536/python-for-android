42804c22d9960c39dd48e8b3a9af2ef40938020b
shimoda@desktop <shimoda@desktop>
2015-04-13 02:27:27 +0900

    Less dependency to the uClibc libcrypt

--- python-build/libcrypt.mk
+++ python-build/libcrypt.mk
@@ -7,7 +7,8 @@ LOCAL_MODULE_FILENAME := libcrypt
 LOCAL_SRC_FILES := crypt.c \
 	des.c \
 	md5.c
-LOCAL_CFLAGS := -D__USE_GNU -D_LIBC  -DHAVE_LIMITS_H -D__GNU_LIBRARY__
+LOCAL_CFLAGS := -D__USE_GNU -D_LIBC  -DHAVE_LIMITS_H -D__GNU_LIBRARY__ \
+                -D__THROW= -Dattribute_hidden= -D"__nonnull(a...)= "
 LOCAL_C_EXPORTS := $(LOCAL_PATH)
 
 include $(BUILD_SHARED_LIBRARY)
