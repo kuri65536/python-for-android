1a90b4398e0e55300517cac16780dcde0a10da45
shimoda@desktop <shimoda@desktop>
2015-05-01 16:24:00 +0900

    Fixed selectmodule to link libm in Android NDK.
    - from http://bugs.python.org/file35490/time_and_select_module_link_with_libm.patch

--- tools/localinstall.sh
+++ tools/localinstall.sh
@@ -2,15 +2,16 @@
 cat <<EOF
 * you should have rooted phone.
 * you must click 1st time.
-* adb: $adb
+* adb:    $adb
 * sdcard: $sd in your device.
+* _2or3:  $_2or3
 EOF
 [ x$sd = x ] && (echo you must specify your sd card from sd; exit 1)
 [ x$adb = x ] && adb=adb
 sdext=/sdcard           # fix to fit your devcie.
-zip_bin=python_$(cat LATEST_VERSION).zip
-zip_ext=python_extras_$(cat LATEST_VERSION_EXTRA).zip
-pkg=com.googlecode.pythonforandroid
+zip_bin=python${_2or3}_$(cat LATEST_VERSION).zip
+zip_ext=python${_2or3}_extras_$(cat LATEST_VERSION_EXTRA).zip
+pkg=com.googlecode.python${_2or3}forandroid
 path_inst=$sd/$pkg
 path_app=/data/data/$pkg/files
 
