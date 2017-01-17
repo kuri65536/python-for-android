d1dd6c8d57394f84159dc62b072d40ed1447f2b9
shimoda <kuri65536@hotmail.com>
2017-01-18 01:40:19 +0900

    launch sl4a server before python start #126

--- sl4atools/python3.sh
+++ sl4atools/python3.sh
@@ -5,14 +5,25 @@ elif ! type \cut > /dev/null; then api=14; else
     api=`grep build.version.sdk /system/build.prop | cut -d = -f 2`
 fi
 
+# launch SL4A server is you want.
+if false; then
+    export AP_PORT='8888' # SL4A Port
+    sl4a=com.googlecode.android_scripting
+    am start -a $sl4a.action.LAUNCH_SERVER -n
+        $sl4a/.activity.ScriptingLayerServiceLauncher --ei
+        $sl4a.extra.USE_SERVICE_PORT $AP_PORT
+    sleep 2
+fi
+
 
 PW=`pwd`
+ver=`python --version | cut -d " "  -f 2 | cut -d . -f 1-2`
 export EXTERNAL_STORAGE=$st
 export LANG=en
 bin=/data/data/com.googlecode.python3forandroid/files/python3
 ext=$st/com.googlecode.python3forandroid/extras/python3
 PYTHONPATH=$ext
-PYTHONPATH=${PYTHONPATH}:$bin/lib/python3.4/lib-dynload
+PYTHONPATH=${PYTHONPATH}:$bin/lib/python$ver/lib-dynload
 export PYTHONPATH
 export TEMP=$ext/tmp
 export PYTHON_EGG_CACHE=$TEMP
